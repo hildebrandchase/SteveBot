bce6dd26f902eb08d6a2a7c861a2261b3ad47bb8
Chase <hildebrandchaseb@gmail.com>
2026-01-07 04:51:52 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from dotenv import load_dotenv
+import datetime
+
+
+load_dotenv()
+
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+        try:
+            guild = discord.Object(id=594913076390789138)
+            synced = await self.tree.sync(guild=guild)
+            print(f"Synced {len(synced)} commands to guild {guild.id}")
+        except Exception as e:
+            print(f"Error syncing commands: {e}")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(command_prefix='!', intents=intents)
+
+GUILD_ID = discord.Object(id=594913076390789138)
+
+@client.tree.command(name='countdown', description='Countdown until release', guild=GUILD_ID)
+async def countdown(interaction: discord.Interaction):
+    now = datetime.datetime.now()
+    release = datetime.datetime(2026, 1, 15, 12)
+    await interaction.response.send_message(f'Hackero Next releases in {release - now}!')
+
+client.run(os.getenv('DISCORD_TOKEN'))
